aad055f6ff7a62fbd1dfa8ff09eda73d3f818f4f
best01211 <jinwoobest.andy01@gmail.com>
2025-04-13 21:44:40 +0900

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import ctypes
-
-# 경로 바꾸지 말고 그대로 실행해 보세요
-try:
-    ctypes.WinDLL(r"C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.2\bin\cudnn_ops_infer64_8.dll")
-    print("✅ DLL 로딩 성공")
-except Exception as e:
-    print("❌ 로딩 실패:", e)
